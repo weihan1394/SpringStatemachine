3087a02b73bf7121eb49f20b1667e36506883b3f
Chia Wei Han <weihan1394@gmail.com>
2019-06-24 17:10:10 +0800

    - Initialise project

---
+++ src/main/java/com/weihan1394/SpringStatemachine/SpringStatemachineApplication.java
@@ -0,0 +1,13 @@
+package com.weihan1394.SpringStatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStatemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStatemachineApplication.class, args);
+	}
+
+}
